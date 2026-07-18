1fd889831b2ccd7edc78df112e13d2a661a88e54
Ryan Caldwell <ryanjcaldwell4@gmail.com>
2026-07-18 15:43:49 -0400

    Improve Neovim change review workflow
    Replace Neo-tree with Snacks Explorer and add Diffview+ for reviewing working-tree changes. All CI checks passed.

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,51 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'dlyongemallo/diffview-plus.nvim',
+    version = '*',
+    cmd = {
+      'DiffviewOpen',
+      'DiffviewClose',
+      'DiffviewToggleFiles',
+      'DiffviewFocusFiles',
+      'DiffviewFileHistory',
+    },
+    keys = {
+      {
+        '<leader>gf',
+        '<cmd>DiffviewOpen HEAD<cr>',
+        desc = 'Review git changes since HEAD',
+      },
+      {
+        '<leader>gq',
+        '<cmd>DiffviewClose<cr>',
+        desc = 'Close git review',
+      },
+    },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        default = {
+          layout = 'diff2_horizontal',
+        },
+      },
+      file_panel = {
+        listing_style = 'tree',
+        win_config = {
+          position = 'left',
+          width = 35,
+        },
+      },
+      hooks = {
+        diff_buf_read = function(bufnr)
+          vim.api.nvim_buf_call(bufnr, function()
+            vim.opt_local.wrap = false
+            vim.opt_local.list = false
+          end)
+        end,
+      },
+    },
+  },
+}
