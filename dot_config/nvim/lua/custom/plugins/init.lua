579a4c0fb6ac39d149ecb035834a75159421d2ba
Ryan Caldwell <ryanjcaldwell4@gmail.com>
2026-07-18 15:42:17 -0400

    fix(ci): format Diffview plugin spec

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -3,50 +3,50 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-	{
-		"dlyongemallo/diffview-plus.nvim",
-		version = "*",
-		cmd = {
-			"DiffviewOpen",
-			"DiffviewClose",
-			"DiffviewToggleFiles",
-			"DiffviewFocusFiles",
-			"DiffviewFileHistory",
-		},
-		keys = {
-			{
-				"<leader>gf",
-				"<cmd>DiffviewOpen HEAD<cr>",
-				desc = "Review git changes since HEAD",
-			},
-			{
-				"<leader>gq",
-				"<cmd>DiffviewClose<cr>",
-				desc = "Close git review",
-			},
-		},
-		opts = {
-			enhanced_diff_hl = true,
-			view = {
-				default = {
-					layout = "diff2_horizontal",
-				},
-			},
-			file_panel = {
-				listing_style = "tree",
-				win_config = {
-					position = "left",
-					width = 35,
-				},
-			},
-			hooks = {
-				diff_buf_read = function(bufnr)
-					vim.api.nvim_buf_call(bufnr, function()
-						vim.opt_local.wrap = false
-						vim.opt_local.list = false
-					end)
-				end,
-			},
-		},
-	},
+  {
+    'dlyongemallo/diffview-plus.nvim',
+    version = '*',
+    cmd = {
+      'DiffviewOpen',
+      'DiffviewClose',
+      'DiffviewToggleFiles',
+      'DiffviewFocusFiles',
+      'DiffviewFileHistory',
+    },
+    keys = {
+      {
+        '<leader>gf',
+        '<cmd>DiffviewOpen HEAD<cr>',
+        desc = 'Review git changes since HEAD',
+      },
+      {
+        '<leader>gq',
+        '<cmd>DiffviewClose<cr>',
+        desc = 'Close git review',
+      },
+    },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        default = {
+          layout = 'diff2_horizontal',
+        },
+      },
+      file_panel = {
+        listing_style = 'tree',
+        win_config = {
+          position = 'left',
+          width = 35,
+        },
+      },
+      hooks = {
+        diff_buf_read = function(bufnr)
+          vim.api.nvim_buf_call(bufnr, function()
+            vim.opt_local.wrap = false
+            vim.opt_local.list = false
+          end)
+        end,
+      },
+    },
+  },
 }
